e9b26389e47ac5cc22d4e82f117cd76c19785673
astonmedia <astonmedia@hotmail.com>
2021-11-18 22:59:49 -0500

    Added averageCost function on the model

--- server.js
+++ server.js
@@ -17,40 +17,40 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 // Dev logging
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("tiny"));
+    app.use(morgan("tiny"));
 }
 // Mount Routers to URLS
 app.use("/api/v1/bootcamps", bootcampRoutes);
 app.use("/api/v1/courses", courseRoutes);
 // Set up opening route
 app.use('/', (req, res) => {
-    res.json({message: 'Loaded API'})
+    res.json({ message: 'Loaded API' })
 })
 // Custom error handler
 app.use(errorHandler);
 // Connect to DB then start server
 const server = async () => {
-  try {
-    // Connection to the database
-    await connectDB();
-    app.listen(
-      PORT,
-      console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port :${PORT}`.yellow
-          .bold
-      )
-    );
-    // Once connected start the server
-  } catch (err) {
-    console.log(`Error: ${err.message}`.red);
-    process.exit(1);
-  }
+    try {
+        // Connection to the database
+        await connectDB();
+        app.listen(
+            PORT,
+            console.log(
+                `Server running in ${process.env.NODE_ENV} mode on port :${PORT}`.yellow
+                    .bold
+            )
+        );
+        // Once connected start the server
+    } catch (err) {
+        console.log(`Error: ${err.message}`.red);
+        process.exit(1);
+    }
 };
 // Start the application
 server();
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-  // Close server and exit process
-  process.exit(1);
+    console.log(`Error: ${err.message}`.red);
+    // Close server and exit process
+    process.exit(1);
 });
