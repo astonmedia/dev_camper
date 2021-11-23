b82b18a8e1077bc52d62d185b81204775bb0142f
astonmedia <astonmedia@hotmail.com>
2021-11-22 22:36:52 -0500

    updated userModel

--- models/userModel.js
+++ models/userModel.js
@@ -33,25 +33,4 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-<<<<<<< HEAD
-=======
-// Encyrpt password using bcrypt
-UserSchema.pre("save", async function (next) {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-// Sign JWT and Return
-UserSchema.methods.getSignedJwtToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
-};
-
-// Match user entered password to hashed password in database
-UserSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
->>>>>>> Register and Login functionally
 module.exports = mongoose.model("User", UserSchema);
