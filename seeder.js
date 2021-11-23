c1e1d6910da1a8888242d19edab1243739eaa253
Ricky Aston <astonmedia@hotmail.com>
2021-11-23 19:05:38 +1000

    added role authorization

--- seeder.js
+++ seeder.js
@@ -9,43 +9,49 @@ dotenv.config({ path: "./config/config.env" });
 // Load Models
 const Bootcamp = require("./models/bootcampModel");
 const Course = require("./models/courseModel");
+const User = require("./models/userModel");
 
 mongoose.connect(process.env.MONGO_URI);
 
 // Read Json Files
 const bootcamps = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 const courses = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
 
 // Import into DB
 const importData = async () => {
-    try {
-        await Bootcamp.create(bootcamps);
-        await Course.create(courses);
-        console.log("Data Imported...".green.inverse);
-        process.exit();
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    await User.create(courses);
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 // Delete Data
 const deleteData = async () => {
-    try {
-        await Bootcamp.deleteMany();
-        await Course.deleteMany();
-        console.log("Data Destroyed...".red.inverse);
-        process.exit();
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    await User.deleteMany();
+    console.log("Data Destroyed...".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 if (process.argv[2] === "-i") {
-    importData();
+  importData();
 } else if (process.argv[2] === "-d") {
-    deleteData();
+  deleteData();
 }
