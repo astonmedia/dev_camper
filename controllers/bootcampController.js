b7557fd56eee170cfc6c2401aa828e329c6e621a
astonmedia <astonmedia@hotmail.com>
2021-11-25 20:32:28 -0500

    Bootcamp and course ownsership

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -60,15 +60,27 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @access Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
   const bootcampId = req.params.id;
-  const bootcamp = await Bootcamp.findByIdAndUpdate(bootcampId, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let bootcamp = await Bootcamp.findById(bootcampId);
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Resource not found with ID of ${req.params.id}`, 404)
     );
   }
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
   res.status(200).json({
     success: true,
     data: bootcamp,
@@ -84,6 +96,15 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Resource not found with ID of ${req.params.id}`, 404)
+    );
+  }
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to delete this bootcamp`,
+        401
+      )
     );
   }
   // Delete Bootcamp from database
@@ -134,6 +155,15 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Resource not found with ID of ${req.params.id}`, 404)
+    );
+  }
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
     );
   }
   // Check to see if file uploaded
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -47,12 +47,23 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
   // Check to see if bootcamp exits
   if (!bootcamp) {
     return next(
       new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),
       404
+    );
+  }
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to this bootcamp ${bootcamp.id}`,
+        401
+      )
     );
   }
   // Create Course
@@ -76,6 +87,15 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
       404
     );
   }
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to  update course ${course.id}`,
+        401
+      )
+    );
+  }
   // Update Course
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -97,6 +117,15 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(
       new ErrorResponse(`Resource not found with ID of ${courseId}`, 404)
+    );
+  }
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add delete course ${course.id}`,
+        401
+      )
     );
   }
   // Delete Course from database
