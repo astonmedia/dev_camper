31905ee3f6fb2834f9f1508f5c03f20b8e9d56ec
ricky aston <rickysimonaston@gmail.com>
2021-12-01 04:45:01 +0000

    bootcamp reviews and ratings

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -58,3 +58,46 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 		data: review,
 	});
 });
+
+// @desc Update Review
+// @route PUT /api/v1/reviews/:id
+// @access Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+	let review = await Review.findById(req.params.id);
+
+	if (!review) {
+		return next(new ErrorResponse(`No review found with the id of ${req.params.id}`, 404));
+	}
+	// Ensure review belongs to user or user is an admin
+	if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse('Not authorised to update review', 401));
+	}
+	review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidation: true,
+	});
+	res.status(201).json({
+		success: true,
+		data: review,
+	});
+});
+
+// @desc Delete Review
+// @route DELETE /api/v1/reviews/:id
+// @access Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+	const review = await Review.findById(req.params.id);
+
+	if (!review) {
+		return next(new ErrorResponse(`No review found with the id of ${req.params.id}`, 404));
+	}
+	// Ensure review belongs to user or user is an admin
+	if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse('Not authorised to update review', 401));
+	}
+	await Review.deleteOne();
+	res.status(201).json({
+		success: true,
+		data: {},
+	});
+});
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router({ mergeParams: true });
-const { getReviews, getReview, addReview } = require('../controllers/reviewController');
+const {
+	getReviews,
+	getReview,
+	addReview,
+	updateReview,
+	deleteReview
+} = require('../controllers/reviewController');
 
 const Review = require('../models/reviewModel');
 const advancedResults = require('../middleware/advancedResults');
@@ -16,6 +22,6 @@ router
 	)
 	.post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id').get(getReview);
+router.route('/:id').get(getReview).put(protect, authorize('user', 'admin'),updateReview).delete(protect, authorize('user', 'admin'),deleteReview);
 
 module.exports = router;
